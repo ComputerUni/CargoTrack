30eb98310cf8b9143ef506a01f9a3297345233d3
Mehmet Kadir Güngör <m.kadir-27@hotmail.com>
2026-08-20 19:47:31 +0300

    first commit

---
+++ CargoTrack/CargoTrack.DataAccess/Repositories/Abouts/AboutRepository.cs
@@ -0,0 +1,15 @@
+﻿using CargoTrack.DataAccess.Context;
+using CargoTrack.DataAccess.Repositories.GenericRepositories;
+using CargoTrack.Entity.Entities;
+
+
+namespace CargoTrack.DataAccess.Repositories.Abouts
+{
+    public class AboutRepository : GenericRepository<About>, IAboutRepository
+    {
+        public AboutRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
+
